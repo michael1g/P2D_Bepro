e50b3ec14ecfb91fb4c8795d999c8620459c8c79
Michael Grossman <mg@beprotravel.com>
2023-06-15 11:39:25 +0300

    Update GenesisBepro.js
    lo

--- GenesisBepro.js
+++ GenesisBepro.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        BePro 2 Genesis!
 // @namespace   https://genesis.beprotravel.com/
-// @version     3.0.2
+// @version     3.0.3
 // @description This userscript send BePro Data to fill some order information in external systems
 // @author      Misha Kav
 // @copyright   2022, BePro Team
@@ -25,7 +25,7 @@
 
   let _Order;
   let SourceUrl = "";
-  let shouldSaveGlobal = true;
+  let shouldSaveGlobal = false;
 
   const TIMEOUT = 420;
 
