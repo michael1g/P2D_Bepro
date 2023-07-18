85a64e533f591241bfa8db95de74be63f7ac208e
Michael Grossman <mg@beprotravel.com>
2023-07-18 17:17:03 +0300

    Update GenesisBepro.js

--- GenesisBepro.js
+++ GenesisBepro.js
@@ -15,8 +15,8 @@
 // @grant       GM_registerMenuCommand
 // @grant       GM_unregisterMenuCommand
 // @run-at      document-end
-// @updateURL   https://raw.githubusercontent.com/michael1g/GenesisBepro/main/GenesisBepro.js
-// @downloadURL https://raw.githubusercontent.com/michael1g/GenesisBepro/main/GenesisBepro.js
+// @updateURL   https://raw.githubusercontent.com/michael1g/P2D_Bepro/main/GenesisBepro.js
+// @downloadURL https://raw.githubusercontent.com/michael1g/P2D_Bepro/main/GenesisBepro.js
 // ==/UserScript==
 
 /* global $, jQuery, NC */
